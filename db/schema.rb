cb234c6c754efe5cb75179530a852ff1257f200b
onizuka <dark_sao@mail.ru>
2023-09-18 10:49:34 +0300

    add model Answer and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_17_145217) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_072437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "question_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -21,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_17_145217) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
